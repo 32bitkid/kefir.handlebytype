ce6fdff86d78b7de30be161f596f2963f73b5043
J. Holmes <jholmes@n.io>
2017-01-28 19:03:02 -0700

    bad decomposition

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ export default
   (receivers, passthrough = true) =>
   (emitter, e1, e2) => {
     const event = (e2 === undefined) ? e1 : e2;
-    const { valueType, ...rest } = (e2 === undefined) ? e1.value : e1;
+    const { type, ...rest } = (e2 === undefined) ? e1.value : e1;
     for (let key in receivers) {
       if (!receivers.hasOwnProperty(key)) { continue; }
-      if (key !== valueType) { continue; }
+      if (key !== type) { continue; }
       receivers[key](emitter, rest, event);
       return;
     }
